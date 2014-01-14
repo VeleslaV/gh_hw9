f57865d91f5ec1402e973714bba606c97efa2cd3
VeleslaV <veleslav.ck@gmail.com>
2014-01-15 00:55:16 +0200

    Simple integration of Article entity in SonataAdminBundle.

---
+++ src/Veles/HomeWorkBundle/Admin/ArticleAdmin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Veles\HomeWorkBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ArticleAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Article Title'))
+            ->add('category', 'entity', array(
+                'class' => 'Veles\HomeWorkBundle\Entity\Category',
+                'property' => 'title'
+            ))
+            ->add('body')
+            ->add('created')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->addIdentifier('title')
+            ->add('created')
+        ;
+    }
+}
